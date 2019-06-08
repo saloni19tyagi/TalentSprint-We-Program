a84d287ecad2ec07640907bc98791d0322a168ec
Saloni Tyagi <saloni19tyagi@gmail.com>
2019-06-08 10:37:34 +0530

    Different implementation

--- assessment_7_6_2019/second_ques.py
+++ assessment_7_6_2019/second_ques.py
@@ -1,26 +1,13 @@
 import sys
 
-def merge(str , it) :
-    merge_str = ""
-    while it < len(str) :
-        merge_str += str[it]
-        it = it+1
-    return merge_str
-
 def interleaving(str1 ,str2) :
-    interleaved_str = ""
-    it1 = 0
-    it2 = 0
-    while it1 < len(str1) and it2 < len(str2) :
-        interleaved_str += str1[it1]
-        interleaved_str += str2[it2]
-        it1 = it1+1 ;
-        it2 = it2+1 ;
+    if len(str1) == 0 :
+        return str2
+    if len(str2) == 0 :
+        return str1
+    else :
+        return str1[0] + str2[0] + interleaving(str1[1:],str2[1:])
 
-    interleaved_str += merge(str1 , it1) ;
-    interleaved_str += merge(str2 , it2) ;
-
-    return interleaved_str ;
 
 
 str1 = sys.argv[1]
