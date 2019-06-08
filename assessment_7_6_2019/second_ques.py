99195aeb6acc195b2da57a584ae02742aadb1201
Saloni Tyagi <saloni19tyagi@gmail.com>
2019-06-08 15:06:04 +0530

    Interleaving of string using ZIP function

--- assessment_7_6_2019/second_ques.py
+++ assessment_7_6_2019/second_ques.py
@@ -1,17 +1,16 @@
 import sys
 
 def interleaving(str1 ,str2) :
-    if len(str1) == 0 :
-        return str2
-    if len(str2) == 0 :
-        return str1
-    else :
-        return str1[0] + str2[0] + interleaving(str1[1:],str2[1:])
-
-
+    li =  list(zip(str1, str2))
+    return "".join([i+j for i, j in li])
 
 str1 = sys.argv[1]
 str2 = sys.argv[2]
 
 interleave = interleaving(str1 , str2)
+if len(str1) > len(str2) :
+    interleave+=(str1[len(str2):])
+else :
+    interleave+=(str2[len(str1):])
+
 print(interleave)
