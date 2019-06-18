cce4f9a675df537c24e0f285d3bca3140a8cabd4
Saloni Tyagi <saloni19tyagi@gmail.com>
2019-06-18 05:03:25 +0000

    Check whether the string is pangram or not

---
+++ Assessment_18_06_2019/pangram.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std ;
+
+bool is_pangram(string str) {
+    int freq[30] = {0};
+    int len = str.size();
+    
+    for (int i = 0; i < len;i++) {
+        freq[str[i] - 'A']++;
+    }
+    for (int i = 0; i < 26; i++) {
+        if(freq[i] == 0)
+            return false;
+    }
+
+        return true;
+}
+
+int main() {
+
+    string str;
+    str = "Pack my box with five dozen liquor jugs";
+    transform(str.begin(), str.end(), str.begin(), ::toupper);
+
+    bool check = is_pangram(str);
+    if(check) {
+        cout << "String is pangram\n";
+    }
+    else {
+        cout << "String is not pangram\n";
+    }
+    return 0;
+}
