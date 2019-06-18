c432c0929277deff8e3a246833d8eda985e574c2
Saloni Tyagi <saloni19tyagi@gmail.com>
2019-06-18 05:06:58 +0000

    Check whether the String is pangram or not in python

---
+++ Assessment_18_06_2019/pangram.py
@@ -0,0 +1,19 @@
+import string
+letter_count = dict(zip(string.ascii_lowercase, [0]*26))
+
+def is_pangram(s) :
+    for ch in s: 
+        letter_count[ch] = letter_count.get(ch, 0) + 1
+    print(letter_count)
+    for count in letter_count :
+        if count == 0 :
+            return 0
+
+    return 1
+
+s = "Pack my box with five dozen liquor jugs"
+s = s.lower() 
+if(is_pangram(s)) :
+    print("String is Pangram") 
+else :
+    print("String is not pangram")
